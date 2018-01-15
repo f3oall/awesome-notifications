401d805d5d39d9726d8b78b3c5244d1990708e21
David Yunevich <f3oall@gmail.com>
2018-01-16 00:16:07 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,78 @@
-// const CssEntryPlugin = require("css-entry-webpack-plugin");
-var path = require("path");
-var webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+var path = require("path")
+var webpack = require("webpack")
 
-module.exports = {
-  entry: {
-    index: "./src/index.js",
-    styles: ["./src/styles/style.scss"]
-  },
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "[name].js",
-    publicPath: "/dist/",
-    library: "vue-aweseome-notifications",
-    libraryTarget: "umd"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].css"
-            }
-          },
-          {
-            loader: "extract-loader"
-          },
-          {
-            loader: "css-loader",
-            options: {
-              minimize: true
-            }
-          },
-          {
-            loader: "sass-loader"
-          }
-        ]
-      },
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        exclude: /node_modules/
-      }
+module.exports = [
+  {
+    entry: ["./src/index.js"],
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: "index.js",
+      library: "AWN",
+      libraryTarget: "umd"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          exclude: /node_modules/
+        }
+      ]
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        include: /\.js$/,
+        minimize: true
+      })
     ]
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      include: /\.js$/,
-      minimize: true
-    })
-  ]
-};
+  {
+    entry: ["./src/index.js"],
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: "index.var.js",
+      library: "AWN",
+      libraryTarget: "var"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          exclude: /node_modules/
+        }
+      ]
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        include: /\.js$/,
+        minimize: true
+      })
+    ]
+  },
+  {
+    entry: ["./src/styles/style.scss"],
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: "style.js",
+      library: "AWN",
+      libraryTarget: "var"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: [
+              { loader: "css-loader", options: { minimize: true } },
+              "sass-loader"
+            ]
+          })
+        }
+      ]
+    },
+    plugins: [new ExtractTextPlugin("style.css")]
+  }
+]
