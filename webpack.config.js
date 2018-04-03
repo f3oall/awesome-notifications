0339f5b68130858f0ee5436768f7df32df5d3367
David Yunevich <f3oall@gmail.com>
2018-04-03 08:26:29 +0300

    update to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 var path = require("path")
 var webpack = require("webpack")
 
-module.exports = [
-  {
-    entry: ["./src/index.js"],
+function createConfig(target, entry = "index") {
+  return {
+    entry: `./src/${entry}.js`,
     output: {
       path: path.resolve(__dirname, "./dist"),
-      filename: "index.js",
+      filename: `${entry}.js`,
       library: "AWN",
-      libraryTarget: "umd"
+      libraryTarget: target
     },
     module: {
       rules: [
@@ -19,60 +19,39 @@ module.exports = [
           exclude: /node_modules/
         }
       ]
-    },
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin({
-        include: /\.js$/,
-        minimize: true
-      })
-    ]
-  },
-  {
-    entry: ["./src/index.var.js"],
-    output: {
-      path: path.resolve(__dirname, "./dist"),
-      filename: "index.var.js",
-      library: "AWN",
-      libraryTarget: "var"
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          loader: "babel-loader",
-          exclude: /node_modules/
-        }
-      ]
-    },
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin({
-        include: /\.js$/,
-        minimize: true
-      })
-    ]
-  },
-  {
-    entry: ["./src/styles/style.scss"],
-    output: {
-      path: path.resolve(__dirname, "./dist"),
-      filename: "style.js",
-      library: "AWN",
-      libraryTarget: "var"
-    },
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: ExtractTextPlugin.extract({
-            fallback: "style-loader",
-            use: [
-              { loader: "css-loader", options: { minimize: true } },
-              "sass-loader"
-            ]
-          })
-        }
-      ]
-    },
-    plugins: [new ExtractTextPlugin("style.css")]
+    }
   }
-]
+}
+var styles = {
+  entry: "./src/styles/style.scss",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "style.js",
+    library: "AWN",
+    libraryTarget: "var"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              minimize: true
+            }
+          },
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css"
+    })
+  ]
+}
+
+module.exports = [createConfig("umd"), createConfig("var", "index.var"), styles]
