94d145ac5d9d63a655a7897eff250d320103f732
David Yunevich <f3oall@gmail.com>
2021-09-28 20:00:25 +0300

    update devDependencies v3.1.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const CompressionPlugin = require('compression-webpack-plugin');
-var path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+var path = require("path");
 
 module.exports = [
   createConfig("umd", "index", ["defaults"]),
@@ -14,56 +14,56 @@ module.exports = [
       path: path.resolve(__dirname, "./dist"),
     },
     module: {
-      rules: [{
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-      }]
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+      ],
     },
     optimization: {
-      minimizer: [
-        new OptimizeCSSAssetsPlugin({})
-      ]
+      minimizer: [new CssMinimizerPlugin()],
     },
     plugins: [
       new MiniCssExtractPlugin({
-        filename: "style.css"
+        filename: "style.css",
       }),
-      new CompressionPlugin()
-    ]
-  }
-]
+      new CompressionPlugin(),
+    ],
+  },
+];
 
 function createConfig(target, entry = "index", browsers, filename) {
-  if (!filename) filename = `${entry}.js`
+  if (!filename) filename = `${entry}.js`;
   return {
     entry: `./src/${entry}.js`,
     output: {
       path: path.resolve(__dirname, "./dist"),
       filename: filename,
       library: "AWN",
-      libraryTarget: target
+      libraryTarget: target,
     },
-    plugins: [
-      new CompressionPlugin()
-    ],
+    plugins: [new CompressionPlugin()],
     module: {
-      rules: [{
-        test: /\.js$/,
-        loader: "babel-loader",
-        options: {
-          "presets": [
-            [
-              "@babel/preset-env",
-              {
-                "targets": {
-                  "browsers": browsers
-                }
-              }
-            ]
-          ]
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    browsers: browsers,
+                  },
+                },
+              ],
+            ],
+          },
+          exclude: /node_modules/,
         },
-        exclude: /node_modules/
-      }]
-    }
-  }
+      ],
+    },
+  };
 }
